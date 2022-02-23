f6b8421a1dea004b32e7db97109ee5b7df26b48e
Adam Zvara <xzvara01@stud.fit.vutbr.cz>
2022-02-23 15:22:18 +0100

    add interpret tests

--- src/tests.sh
+++ src/tests.sh
@@ -12,38 +12,102 @@ PARSE_TESTS="$TESTS/parse-only"
 JEXAMXML_PATH="$TESTS/jexamxml/jexamxml.jar"
 JEXAMXML_OPT_PATH="$TESTS"
 
-echo -e "${ORANGE}PARSE-ONLY${NC}"
-for DIR in $(ls $PARSE_TESTS)
-do
-    echo -e "  ${CYAN}$DIR${NC}"
-    for TEST_NAME in $(ls $PARSE_TESTS/$DIR | grep ".src")
+INTERPRET_TESTS="$TESTS/interpret-only"
+
+declare -i TEST_COUNT=0
+declare -i TEST_PASSED=0
+
+function parser_test()
+{
+    echo -e "${ORANGE}PARSE-ONLY${NC}"
+    for DIR in $(ls $PARSE_TESTS)
     do
-        TEST_NAME=${TEST_NAME%.*}
-        echo -e -n "\t${BLUE}$TEST_NAME:${NC}"
+        echo -e "  ${CYAN}$DIR${NC}"
+        for TEST_NAME in $(ls $PARSE_TESTS/$DIR | grep ".src")
+        do
+            TEST_COUNT+=1
+            TEST_NAME=${TEST_NAME%.*}
+            echo -e -n "\t${BLUE}$TEST_NAME:${NC}"
 
-        php8.1 parse.php < "$PARSE_TESTS/$DIR/$TEST_NAME.src" > tmp
-        RETVAL=$?
-        RETVAL_EXPECTED=$(cat "$PARSE_TESTS/$DIR/$TEST_NAME.rc")
+            php8.1 parse.php < "$PARSE_TESTS/$DIR/$TEST_NAME.src" > tmp
+            RETVAL=$?
+            RETVAL_EXPECTED=$(cat "$PARSE_TESTS/$DIR/$TEST_NAME.rc")
 
-        # check if return codes match
-        if [ $RETVAL != $RETVAL_EXPECTED ]; then
-            echo -e " ${RED}FAILED${NC}"
-        else
-            if [ $RETVAL != 0 ]; then
-                # error occured, no need to do XML_DIFF
-                echo -e " ${GREEN}PASSED${NC}"
+            # check if return codes match
+            if [ $RETVAL != $RETVAL_EXPECTED ]; then
+                echo -e " ${RED}FAILED${NC}"
             else
-                # no error occured, do XML_DIFF
-                XML_EXP=$PARSE_TESTS/$DIR/$TEST_NAME.out
-                java -jar $JEXAMXML_PATH tmp $XML_EXP $JEXAMXML_OPT_PATH/options>/dev/null
-                if [ $? -eq 0 ]; then
+                if [ $RETVAL != 0 ]; then
+                    # error occured, no need to do XML_DIFF
                     echo -e " ${GREEN}PASSED${NC}"
+                    TEST_PASSED+=1
                 else
-                    echo -e " ${RED}FAILED${NC}"
+                    # no error occured, do XML_DIFF
+                    XML_EXP=$PARSE_TESTS/$DIR/$TEST_NAME.out
+                    java -jar $JEXAMXML_PATH tmp $XML_EXP $JEXAMXML_OPT_PATH/options>/dev/null
+                    if [ $? -eq 0 ]; then
+                        TEST_PASSED+=1
+                        echo -e " ${GREEN}PASSED${NC}"
+                    else
+                        echo -e " ${RED}FAILED${NC}"
+                    fi
                 fi
             fi
-        fi
+        done
     done
-done
+}
+
+function interpret_test()
+{
+    echo -e "${ORANGE}INTERPRET-ONLY${NC}"
+    for DIR in $(ls $INTERPRET_TESTS)
+    do
+        echo -e "  ${CYAN}$DIR${NC}"
+        for TEST_NAME in $(ls $INTERPRET_TESTS/$DIR | grep ".src")
+        do
+            TEST_COUNT+=1
+            TEST_NAME=${TEST_NAME%.*}
+            echo -e -n "\t${BLUE}$TEST_NAME:${NC}"
+
+            src=$INTERPRET_TESTS/$DIR/$TEST_NAME'.src'
+            input=$INTERPRET_TESTS/$DIR/$TEST_NAME'.in'
+            python3 interpret.py --source=$src --input=$input > tmp
+            RETVAL=$?
+            RETVAL_EXPECTED=$(cat "$INTERPRET_TESTS/$DIR/$TEST_NAME.rc")
+
+            # check if return codes match
+            if [ $RETVAL != $RETVAL_EXPECTED ]; then
+                echo -e " ${RED}FAILED${NC}"
+            else
+                if [ $RETVAL != 0 ]; then
+                    # error occured, no need to check output
+                    TEST_PASSED+=1
+                    echo -e " ${GREEN}PASSED${NC}"
+                else
+                    diff $INTERPRET_TESTS/$DIR/$TEST_NAME.'out' tmp >/dev/null
+                    if [ $? -eq 0 ]; then
+                       TEST_PASSED+=1
+                        echo -e " ${GREEN}PASSED${NC}"
+                    else
+                        echo -e " ${RED}FAILED${NC}"
+                    fi
+                fi
+            fi
+
+        done
+    done
+}
+
+
+if [ -z $1 ]; then
+    parser_test
+    interpret_test
+elif [ $1 == 'parser' ]; then
+    parser_test
+elif [ $1 == 'interpret' ]; then
+    interpret_test
+fi
+
+echo "Passed" $TEST_PASSED "tests out of" $TEST_COUNT
 
 rm -rf tmp*
