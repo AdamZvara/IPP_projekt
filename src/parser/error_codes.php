16fbdcb63c88af85b554ce86806e21cbf13a16c6
Adam Zvara <xzvara01@stud.fit.vutbr.cz>
2022-03-05 13:03:33 +0100

    missing comma fix

--- src/parser/error_codes.php
+++ src/parser/error_codes.php
@@ -5,5 +5,5 @@ if (!defined('E_OUTFILE'))  define('E_OUTFILE',  12);
 if (!defined('E_HEADER'))   define('E_HEADER',   21);
 if (!defined('E_OPCODE'))   define('E_OPCODE',   22);
 if (!defined('E_OTHER'))    define('E_OTHER',    23);
-if (!defined('E_INTERNAL')) define('E_INTERNAL', 99)
+if (!defined('E_INTERNAL')) define('E_INTERNAL', 99);
 ?>
